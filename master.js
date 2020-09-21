c1dba632b0fc9b10c9c5b4eacf83148e7e976f29
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-21 07:47:12 -0500

    Pretty much finished

--- master.js
+++ master.js
@@ -1,3 +1,13 @@
+var size = 600, //squared
+    gridSize = 25,
+    speed = 4,
+    c = 0,
+    d,
+    food,
+    snake,
+    n,
+    WIDTH, HEIGHT;
+
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
@@ -10,6 +20,11 @@ function init() {
     WIDTH = window.innerWidth;
     HEIGHT = window.innerHeight;
 
+    if (WIDTH >= size) {
+        WIDTH = size;
+        HEIGHT = WIDTH;
+    }
+
     canvas.setAttribute('width', WIDTH);
     canvas.setAttribute('height', HEIGHT);
 
@@ -19,13 +34,133 @@ function init() {
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     ctx.closePath();
 
+    n = size / gridSize;
+
+    ctx.strokeStyle = 'white';
+    ctx.font = "30px sans-serif";
+
+    var ix = Math.floor(n / 2) * gridSize;
+    var iy = Math.floor(n / 2) * gridSize;
+
+    d = Math.floor(random(4));
+
+    var x, y;
+
+    switch (d) {
+        case 0:
+            x = gridSize;
+            y = 0;
+            break;
+        case 1:
+            x = 0;
+            y = gridSize;
+            break;
+        case 2:
+            x = -gridSize;
+            y = 0;
+            break;
+        case 3:
+            x = 0;
+            y = -gridSize;
+            break;
+    }
+
+    document.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case "ArrowRight":
+                x = gridSize;
+                y = 0;
+                break;
+            case "ArrowDown":
+                x = 0;
+                y = gridSize;
+                break;
+            case "ArrowLeft":
+                x = -gridSize;
+                y = 0;
+                break;
+            case "ArrowUp":
+                x = 0;
+                y = -gridSize;
+                break;
+        }
+        let newDir = new Vector(x, y);
+
+        if (legalDir(newDir)) {
+            snake.nextDir.set(x, y);
+        }
+    });
+
+    d = new Vector(x, y);
+
+    snake = new Snake(ix, iy, gridSize, d);
+    snake.nextDir = d.copy();
+
+    food = newFood();
+
     ani();
 }
 
 function ani() {
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
+    if (!snake.dead) {
+        drawFood();
 
-    //   requestAnimationFrame(ani);
+        snake.update();
+        snake.draw();
+        snake.ate();
+    } else {
+        ctx.save();
+        ctx.fillStyle = "white";
+        ctx.fillText("You lost!", WIDTH / 2 - 40, 100);
+        ctx.font = "20px sans-serif";
+        ctx.fillText("Click to restart", WIDTH / 2 - 48, 300);
+        ctx.restore();
+
+        window.addEventListener('mousedown', handleClick, true);
+    }
+
+    c++;
+    setTimeout(ani, 1000 / speed);
 }
 
 init();
+
+function drawFood() {
+    rect(food.x, food.y, gridSize, gridSize, false, true, "#f66");
+}
+
+function newFood() {
+    // TODO: check snake segments
+    let f = new Vector(
+        Math.floor(random(n)) * gridSize,
+        Math.floor(random(n)) * gridSize
+    );
+
+    speed += 0.1;
+
+    return f;
+}
+
+function legalDir(newDir) {
+    if (newDir.x == snake.dir.x && newDir.y == snake.dir.y) {
+        return false;
+    } else if (newDir.x == -snake.dir.x) {
+        return false;
+    } else if (newDir.y == -snake.dir.y) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function handleClick() {
+    var ix = Math.floor(n / 2) * gridSize;
+    var iy = Math.floor(n / 2) * gridSize;
+
+    snake = new Snake(ix, iy, gridSize, d);
+    snake.nextDir = d.copy();
+
+    window.removeEventListener("mousedown", handleClick, true);
+}
---
+++ snake.js
@@ -0,0 +1,69 @@
+class Snake {
+    constructor(x, y, u, dir) {
+        this.segments = [new Vector(x, y)];
+        this.u = u;
+        this.dir = dir;
+        this.nextDir = new Vector();
+        this.len = 1;
+        this.ns = []; // next segments
+        this.dead = false;
+    }
+
+    draw() {
+        for (var i = 0; i < this.segments.length; i++) {
+            let seg = this.segments[i];
+
+            rect(seg.x, seg.y, this.u, this.u);
+        }
+    }
+
+    update() {
+        if (this.segments.length > 1) {
+            for (var i = this.segments.length - 1; i > 0; i--) {
+                this.segments[i] = this.segments[i - 1].copy();
+            }
+        }
+
+        this.dir = this.nextDir.copy();
+
+        this.segments[0].add(this.dir);
+
+        if (this.ns.length) {
+            let c = 0;
+            let check = this.ns[0];
+
+            for (var i = 0; i < this.segments.length - 1; i++) {
+                let seg = this.segments[i];
+
+                if (seg.x == check.x && seg.y == check.y) {
+                    c++;
+                }
+            }
+
+            if (c == 0) {
+                this.segments.push(this.ns[0]);
+                this.ns.shift();
+            }
+        }
+
+        let head = this.segments[0];
+        for (var i = 1; i < this.segments.length; i++) {
+            if (head.x == this.segments[i].x && head.y == this.segments[i].y) {
+                this.dead = true;
+            }
+        }
+
+        if (head.x > WIDTH || head.x < 0 || head.y > HEIGHT || head.y < 0) {
+            this.dead = true;
+        }
+    }
+
+    ate() {
+        let head = this.segments[0];
+
+        if (head.x == food.x && head.y == food.y) {
+            this.ns.push(food);
+            food = newFood();
+        }
+    }
+}
